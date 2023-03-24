ece1431a2215b038bdfb34f4214583c75f46ee30
ridzwan-gigih-herdyantha <ridzwangigih3@gmail.com>
2023-03-24 15:53:15 +0700

    Faster Insert Data with Array Chunk

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Factory;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Factory::create();
+        for ($i=0;$i < 100; $i++ )
+        {
+            $data[] = [
+                'uuid' => Str::uuid(),
+                'name' => $faker->name(),
+                'email' => $faker->unique()->safeEmail(),
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+            ];
+        }
+
+        foreach (array_chunk($data, 10) as $item) {
+            User::insert($item);
+        }
+
+    }
+}
