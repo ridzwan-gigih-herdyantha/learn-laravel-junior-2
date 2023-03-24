0bbe2c737146c634397087dab8271b952e48b338
ridzwan-gigih-herdyantha <ridzwangigih3@gmail.com>
2023-03-24 14:31:18 +0700

    UUID #8

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -15,17 +16,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
         // Post::factory(5)->create();
         // Post::create([
         //     'title' => 'Ini adalah title 1' , 
         //     'excerpt' => 'Ini adalah excerpt',
+        //     'slug' => 'ini-slug',
         //     'body' => 'iniadalahbodyiniadalahbodyiniadalahbodyiniadalahbodyiniadalahbodyiniadalahbodyiniadalahbody'
         // ]);
         // Post::create([
         //     'title' => 'Ini adalah title 2' , 
         //     'excerpt' => 'Ini adalah excerpt 2',
+        //     'slug' => 'ini-slug',
         //     'body' => 'iniadalahbody2iniadalahbody2iniadalahbody2iniadalahbody2iniadalahbody2iniadalahbody2'
         // ]);
+
     }
 }
